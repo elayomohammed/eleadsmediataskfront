9ca672a12378cef992d2c3612aeee5b24fc9d0ac
Mohammed Elayo Ahmed <52963955+elayomohammed@users.noreply.github.com>
2023-12-09 15:46:33 +0100

    minor fixes

--- src/resources/components/UsersDetailForm.jsx
+++ src/resources/components/UsersDetailForm.jsx
@@ -19,6 +19,46 @@ const UsersDetailForm = () => {
     // Auth0 variables
     const { user, isAuthenticated, isLoading } = useAuth0();
 
+    const detailForm =
+    <form onSubmit={handleSubmitBTN} className="container userDetailsFormControl" id="userDetailsForm">
+        <h3 className="text-center">Please  enter your details</h3>
+        <div className="form-group row">
+            <div className="col">
+                <input type="text" className="input-small form-control form-control-lg" name="fName" placeholder="first name" onChange={handleUserInput} required />
+            </div>
+            <div className="col">
+                <input type="text" className="input-small form-control form-control-lg" name="lName" placeholder="last name" onChange={handleUserInput} required />
+            </div>
+        </div><br /> <br />
+        <div className="form-group row">
+            <div className="col">
+                <input type="email" className="input-small form-control form-control-lg" name="email" placeholder="email address" onChange={handleUserInput} required />
+            </div>
+        </div><br /> <br />
+        <div className="form-group row">
+            <div className="col">
+                <input type="date" className="input-small form-control" name="dob" placeholder="date of birth" onChange={handleUserInput} required />
+            </div>
+            <div className="col">
+                <input type="phone" className="input-small form-control" name="phone" placeholder="phone number" onChange={handleUserInput} required />
+            </div>
+        </div><br /> <br />
+        <div className="form-group row">
+            <div className="col">
+                <input type="text" className="input-small form-control" name="language" placeholder="language" onChange={handleUserInput} required />
+            </div>
+            <div className="col">
+                <label htmlFor="userID"><input type="file" className="input-small form-control" name="userID" id="userID" accept=".jpeg, .png, .pdf" onChange={handleUserInput} required /> Upload your ID</label>
+            </div>
+        </div><br />
+        <div className="form-group text-center">
+            <input type="checkbox" id="terms" name="termStatus" required /><label htmlFor="terms">Accept terms and conditions</label>
+        </div>
+        <div className="form-group text-center">
+            <input type="submit" value="Submit" className="btn-lg btn btn-active" name="id" style={{backgroundColor: '#3C4FAF'}} />
+        </div>
+    </form>
+
     // handling inputs
     const handleUserInput = async (event) => {
         const {name, value, type, files} = event.target;
@@ -90,45 +130,8 @@ const UsersDetailForm = () => {
         isAuthenticated && (document.getElementById('login').style.display = 'none') && (
             <div>
                 <h5>Welcome {user.name}</h5><br />
-                <form onSubmit={handleSubmitBTN} className="container userDetailsFormControl" id="userDetailsForm">
-                    <h3 className="text-center">Please  enter your details</h3>
-                    <div className="form-group row">
-                        <div className="col">
-                            <input type="text" className="input-small form-control form-control-lg" name="fName" placeholder="first name" onChange={handleUserInput} required />
-                        </div>
-                        <div className="col">
-                            <input type="text" className="input-small form-control form-control-lg" name="lName" placeholder="last name" onChange={handleUserInput} required />
-                        </div>
-                    </div><br /> <br />
-                    <div className="form-group row">
-                        <div className="col">
-                            <input type="email" className="input-small form-control form-control-lg" name="email" placeholder="email address" onChange={handleUserInput} required />
-                        </div>
-                    </div><br /> <br />
-                    <div className="form-group row">
-                        <div className="col">
-                            <input type="date" className="input-small form-control" name="dob" placeholder="date of birth" onChange={handleUserInput} required />
-                        </div>
-                        <div className="col">
-                            <input type="phone" className="input-small form-control" name="phone" placeholder="phone number" onChange={handleUserInput} required />
-                        </div>
-                    </div><br /> <br />
-                    <div className="form-group row">
-                        <div className="col">
-                            <input type="text" className="input-small form-control" name="language" placeholder="language" onChange={handleUserInput} required />
-                        </div>
-                        <div className="col">
-                            <label htmlFor="userID"><input type="file" className="input-small form-control" name="userID" id="userID" accept=".jpeg, .png, .pdf" onChange={handleUserInput} required /> Upload your ID</label>
-                        </div>
-                    </div><br />
-                    <div className="form-group text-center">
-                        <input type="checkbox" id="terms" name="termStatus" required /><label htmlFor="terms">Accept terms and conditions</label>
-                    </div>
-                    <div className="form-group text-center">
-                        <input type="submit" value="Submit" className="btn-lg btn btn-active" name="id" style={{backgroundColor: '#3C4FAF'}} />
-                    </div>
-                </form>
-                {handleSubmitBTN? <AllUsersEntryViewModal /> : ''}
+                <detailForm />
+                {handleSubmitBTN? <AllUsersEntryViewModal /> : <detailForm />}
             </div>
         )
     )
